9aeb8bca9af2142695161e64fd1bc895a8a86cc6
Handwoong <handwoong@gmail.com>
2023-12-09 03:15:33 +0900

    [BE] 환경 설정 (#4)
    백엔드 작업 환경을 구성한다.

---
+++ backend/src/main/java/com/handwoong/everyonewaiter/EveryoneWaiterApplication.java
@@ -0,0 +1,11 @@
+package com.handwoong.everyonewaiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryoneWaiterApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(EveryoneWaiterApplication.class, args);
+    }
+}
