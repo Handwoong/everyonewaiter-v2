7d47239ca4b4b853e9b58adf2de19a55db3f64f8
Handwoong <handwoong@gmail.com>
2023-12-15 07:55:34 +0900

    feat(account): 회원가입 및 로그인 페이지 디자인 (#6)

---
+++ frontend/src/routes/accounts/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from '$components/accounts/Header.svelte';
+</script>
+
+<div class="app" id="app">
+	<div class="login login-v1">
+		<div class="login-container">
+			<Header />
+			<div class="login-body">
+				<div class="login-content fs-13px">
+					<slot />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ frontend/src/routes/accounts/login/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>로그인 | 모두의 웨이터</title>
+</svelte:head>
+
+<script lang="ts">
+	import FormInput from '$components/FormInput.svelte';
+</script>
+
+<form action="index.html" method="GET">
+	<FormInput id="email" label="이메일" type="email" />
+	<FormInput id="password" label="비밀번호" type="password" />
+	<div class="login-buttons">
+		<button class="btn btn-theme h-45px d-block w-100 btn-lg" type="submit">로그인</button>
+	</div>
+</form>
+<br />
+<a href="/accounts/register">아직 회원이 아니신가요?</a>
