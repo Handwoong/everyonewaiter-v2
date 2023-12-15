f3b27829c6725bc1dc731f3bb3cd1b53552bde3d
Handwoong <handwoong@gmail.com>
2023-12-16 05:50:24 +0900

    feat(member): 회원가입 기능 구현

--- backend/src/main/java/com/handwoong/everyonewaiter/domain/member/model/Password.java
+++ backend/src/main/java/com/handwoong/everyonewaiter/domain/member/model/Password.java
@@ -5,6 +5,7 @@ import com.handwoong.everyonewaiter.exception.EveryoneWaiterException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.util.regex.Pattern;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.StringUtils;
 
 @Embeddable
@@ -12,7 +13,7 @@ public record Password(
         @Column(nullable = false)
         String password
 ) {
-    public static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@!%*#?&])[A-Za-z\\d@!%*#?&]{8,}$";
+    public static final String PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$";
     private static final Pattern PASSWORD_PATTERN = Pattern.compile(PASSWORD_REGEX);
 
     public Password {
@@ -22,13 +23,18 @@ public record Password(
 
     private void validateNull(final String password) {
         if (!StringUtils.hasText(password)) {
-            throw new EveryoneWaiterException(ErrorCode.INVALID_EMAIL, password);
+            throw new EveryoneWaiterException(ErrorCode.INVALID_PASSWORD, password);
         }
     }
 
     private void validateFormat(final String password) {
         if (!PASSWORD_PATTERN.matcher(password).matches()) {
-            throw new EveryoneWaiterException(ErrorCode.INVALID_EMAIL, password);
+            throw new EveryoneWaiterException(ErrorCode.INVALID_PASSWORD, password);
         }
+    }
+
+    public Password encode(final PasswordEncoder passwordEncoder) {
+        final String encodedPassword = passwordEncoder.encode(password);
+        return new Password(encodedPassword);
     }
 }
--- backend/src/main/java/com/handwoong/everyonewaiter/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/handwoong/everyonewaiter/domain/member/repository/MemberRepository.java
@@ -1,9 +1,15 @@
 package com.handwoong.everyonewaiter.domain.member.repository;
 
+import com.handwoong.everyonewaiter.domain.member.model.Email;
 import com.handwoong.everyonewaiter.domain.member.model.Member;
+import com.handwoong.everyonewaiter.domain.member.model.PhoneNumber;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
+    Optional<Member> findByEmail(Email email);
+
+    boolean existsByEmail(Email email);
+
+    boolean existsByPhoneNumber(PhoneNumber phoneNumber);
 }
