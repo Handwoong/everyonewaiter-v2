6406d7cd330018179b09554b38b657c158478835
Handwoong <handwoong@gmail.com>
2023-12-17 07:21:52 +0900

    feat(member): 로그인 기능 구현

--- backend/src/main/java/com/handwoong/everyonewaiter/domain/member/service/MemberService.java
+++ backend/src/main/java/com/handwoong/everyonewaiter/domain/member/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.handwoong.everyonewaiter.domain.member.service;
 
+import com.handwoong.everyonewaiter.config.security.jwt.TokenResponse;
+import com.handwoong.everyonewaiter.domain.member.dto.MemberLoginRequest;
 import com.handwoong.everyonewaiter.domain.member.dto.MemberRegisterRequest;
 
 public interface MemberService {
     void register(MemberRegisterRequest request);
+
+    TokenResponse login(MemberLoginRequest request);
 }
--- backend/src/main/java/com/handwoong/everyonewaiter/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/handwoong/everyonewaiter/domain/member/service/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package com.handwoong.everyonewaiter.domain.member.service;
 
+import com.handwoong.everyonewaiter.config.security.jwt.GrantType;
+import com.handwoong.everyonewaiter.config.security.jwt.JwtTokenProvider;
+import com.handwoong.everyonewaiter.config.security.jwt.TokenResponse;
+import com.handwoong.everyonewaiter.domain.member.dto.MemberLoginRequest;
 import com.handwoong.everyonewaiter.domain.member.dto.MemberRegisterRequest;
 import com.handwoong.everyonewaiter.domain.member.model.Email;
 import com.handwoong.everyonewaiter.domain.member.model.Member;
@@ -9,6 +13,9 @@ import com.handwoong.everyonewaiter.domain.member.repository.MemberRepository;
 import com.handwoong.everyonewaiter.exception.ErrorCode;
 import com.handwoong.everyonewaiter.exception.EveryoneWaiterException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +26,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationBuilder;
 
     @Override
     @Transactional
@@ -29,6 +38,14 @@ public class MemberServiceImpl implements MemberService {
         final Member member = Member.create(request, passwordEncoder);
         member.changeStatus(MemberStatus.ACTIVE);
         memberRepository.save(member);
+    }
+
+    @Override
+    public TokenResponse login(final MemberLoginRequest request) {
+        final UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        final Authentication authentication = authenticationBuilder.getObject().authenticate(authenticationToken);
+        return tokenProvider.createToken(GrantType.BEARER, authentication);
     }
 
     private void validateDuplicateEmail(final String email) {
